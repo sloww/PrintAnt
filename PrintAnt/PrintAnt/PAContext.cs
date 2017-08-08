de54d14991c214b15a7345d0eeea616400b25f66
李建伟 <hust.sh@gmail.com>
2017-08-09 00:49:15 +0800

    增加了 context文件

---
+++ PrintAnt/PrintAnt/PAContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAnt
+{
+    class PAContext : DbContext
+    {
+        public PAContext()
+            {
+            }
+        public DbSet<Model.DocTemplate> DocTemplates;
+
+    }
+}
