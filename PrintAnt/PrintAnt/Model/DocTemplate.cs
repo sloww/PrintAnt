fdec73ad3d208f2d3a3507613105ee6e155c1b3d
李建伟 <hust.sh@gmail.com>
2017-08-09 00:38:38 +0800

    尝试添加了xml模板类
    1 尝试添加了xml模板类
    2  添加了 EF

---
+++ PrintAnt/PrintAnt/Model/DocTemplate.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PrintAnt.Model
+{
+    /// <summary>
+    /// 用来定义文档的外观的结构 主要是描述xml
+    /// </summary>
+    [Table("PADocTemplate", Schema = "dbo")]
+    class DocTemplate
+    {
+        int ID;
+        [Required]
+        Guid DocGuid;
+        [Required]
+        string PinterFullName;
+        [Required]
+        string DocTemplateName;
+        [Required]
+        string Xml;
+        [Required]
+        DateTime CreateDateTime;
+    }
+}
